49e544bdda12f49ddcd5ca75a178b10bdc52ae3e
yutaka <jyoupf@gmail.com>
2020-08-10 22:05:47 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Hello, world!</h1>
-        <p>React Sample App</p>
-      </React.Fragment>
-    )
-  }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>Hello, world!</h1>
+//         <p>React Sample App</p>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!!</div>
 }
 
 export default App;
